69435aee87656f23e45ee3c17855c49e042ef1e6
Vinz <vinzmykodelrosario@gmail.com>
2025-07-24 20:26:12 +0100

    feat(cli): update 'help' command and flag to show only relevant information
    Remove redundant information like 'completion' command and 'Usage: mdello [flags]' as I do not have any other flags apart form '--help'.

--- cli/root.go
+++ cli/root.go
@@ -24,10 +24,37 @@ func Execute(trelloAPIKey string, configuration *config.Config) {
 	apiKey = trelloAPIKey
 	cfg = configuration
 
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
 	rootCmd.AddCommand(initCmd)
 	rootCmd.AddCommand(boardsCmd)
 	rootCmd.AddCommand(boardCmd)
 	rootCmd.AddCommand(openCmd)
+
+	rootCmd.SetUsageTemplate(
+		`Usage:
+  {{.CommandPath}} [command]{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
